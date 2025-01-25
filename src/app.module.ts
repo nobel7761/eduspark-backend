6a837b777c4d29943a045fc3a80a95f18fa5b370
nobel912 <nobel@grype.ca>
2025-01-25 16:21:47 +0600

    initial setup done with mongodb database

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection, ConnectionStates } from 'mongoose';
+
+@Injectable()
+export class AppService {
+  constructor(@InjectConnection() private connection: Connection) {}
+
+  getHello(): string {
+    const isConnected =
+      this.connection.readyState === ConnectionStates.connected;
+    return `Hello World! MongoDB ${isConnected ? 'is' : 'is not'} connected`;
+  }
+}
