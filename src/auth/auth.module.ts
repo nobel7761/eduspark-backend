01f0a6e58c0d3fc2581fbff621fc6952fb830f54
nobel912 <nobel@grype.ca>
2025-01-27 00:54:49 +0600

    env is working

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -13,9 +14,12 @@ import { JwtRefreshAuthGuard } from './guards/jwt-refresh-auth.guard';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'edusparksecret',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './all-exeption-filters';
-import { AppClusterService } from './cluster.service';
+// import { AppClusterService } from './cluster.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -19,20 +19,17 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new AllExceptionsFilter());
+  const port = process.env.PORT ?? 3333;
   app.enableCors({ origin: '*' });
-
-  await app.listen(process.env.PORT ?? 3333);
-
+  await app.listen(port);
   Logger.warn(
-    `🚀 Application is running on: http://localhost:${process.env.PORT ?? 3333}/${globalPrefix}`,
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
-if (process.env.NODE_ENV === 'production') {
-  AppClusterService.clusterize(bootstrap);
-} else {
-  bootstrap().catch((error) => {
-    Logger.error('Failed to start application:', error);
-    process.exit(1);
-  });
-}
+// AppClusterService.clusterize(bootstrap);
+
+bootstrap().catch((error) => {
+  Logger.error('Failed to start application:', error);
+  process.exit(1);
+});
