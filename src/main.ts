2f4d6b601f1ee62a430fb83f42e8ad1f1d3159c1
nobel912 <nobel@grype.ca>
2025-02-22 02:56:07 +0600

    configuration changed

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(process.env.PORT || 3333);
 }
 
 void bootstrap().catch((error) => {
